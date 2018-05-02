bff172446ceaa237a52da0fc307739f3e95d761b
Garrett Genz <genzg@onid.orst.edu>
2018-05-02 09:07:03 -0700

    Add Draftkings position and sort in DESC order

--- GetNBAProjections.py
+++ GetNBAProjections.py
@@ -1,15 +1,20 @@
 import config
 
 def lambda_handler(event, context):
-    query = """SELECT	team_abbrev, playerid, player_name, min, pts_per_min, CAST(game_date AS VARCHAR(10))
-                FROM	playerprojections
-                WHERE	gameid IN (SELECT gameid FROM games WHERE date(game_date) = '4/09/2018')"""
+    query = """SELECT	pp.team_abbrev, pp.playerid, pp.player_name, dk.position, pp.min, pp.pts_per_min, CAST(pp.game_date AS VARCHAR(10))
+                FROM	draftkings dk JOIN playerprojections pp ON (REPLACE(dk.name, '.', '') = REPLACE(player_name, '.', '') AND upper(dk.team) = team_abbrev AND dk.game_date = pp.game_date)
+                WHERE	pp.gameid IN (SELECT gameid FROM games WHERE date(game_date) = '%s')
+				ORDER BY pp.proj_pts DESC""" % event['game_date']
 
     conn = config.make_conn()
 
     result = config.fetch_data(conn, query)
     conn.close()
 
-    print result
+    return result
 
-lambda_handler(1, 1)
+### Testing ###
+
+#event = {"game_date": "04/10/2018"}
+
+#lambda_handler(event, 1)
